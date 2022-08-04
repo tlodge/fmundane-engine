93a49278abe2794135e7e686a9c1cff7bc8971fd
tlodge <tlodge@gmail.com>
2022-08-04 13:43:18 +0100

    webapp updates

--- drivers/screen_driver/client/src/QRCode.js
+++ drivers/screen_driver/client/src/QRCode.js
@@ -5,7 +5,7 @@ import ReactQRCode from "react-qr-code";
 function QRCode({qrcode="http://www.google.com"}) {
 
     return <div style={{background:"black", width:"100vw", height:"100vh", display:"flex", alignItems:"center", justifyContent:"center"}}>
-        <ReactQRCode value={qrcode} size={300}/>
+        <ReactQRCode value={qrcode} size={900}/>
     </div>
 }
 
